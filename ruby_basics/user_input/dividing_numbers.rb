720f8fa3fefd4df0ec5489575c44a33ed328922b
sashaprodan <prodan.sasha@gmail.com>
2017-03-20 19:13:06 -0500

    User input/ Dividing two numbers

---
+++ ruby_basics/user_input/dividing_numbers.rb
@@ -0,0 +1,30 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
+end
+
+numerator = nil
+denominator = nil
+
+loop do
+  puts ">> Please enter the numerator:"
+  numerator = gets.chomp
+
+  break if valid_number?(numerator)
+  puts ">> Invalid input. Only integers are allowed."
+end
+
+loop do
+  puts ">> Please enter the denominator:"
+  denominator = gets.chomp
+
+  if denominator == "0"
+    puts ">> Invalid input. A denominator of 0 is not allowed."
+  elsif !valid_number?(denominator)
+    puts ">> Invalid input. Only integers are allowed."
+  else
+    break
+  end
+end
+
+division = numerator.to_i / denominator.to_i
+puts ">> #{numerator} / #{denominator} is #{division}"
